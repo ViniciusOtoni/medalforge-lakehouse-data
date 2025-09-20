4054df9ce30c57fa08e4d8460cb5e5e3d352b44c
ViniciusOtoni <viniciusotonimsv@gmail.com>
2025-09-19 21:26:40 -0300

    teste para import relativo

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,7 @@ import sys
 import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-root_dir = os.path.abspath(os.path.join(current_dir, '..', '..'))
+root_dir = os.path.abspath(os.path.join(current_dir, '..', '..', '..'))
 if root_dir not in sys.path:
     sys.path.insert(0, root_dir)
 
